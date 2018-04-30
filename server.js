d0f3062e95c7cca9e9a53ad110f895ab59b2c4b2
narfco <carlos.corzo@outlook.es>
2018-04-30 18:28:35 -0500

    Calculadora

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 7593;
+const HOST = 'localhost';
+
+// App
+const app = express();
+app.get('/api/v1/calculator/add/(((-?[0-9]+)[/]*))+', (req, res) => {
+   var numeros = req.params[0].split('/');
+   var resultado = 0;
+
+   for (var i in numeros){
+       resultado += parseInt(numeros[i]);
+   }
+
+  res.send(resultado.toString());
+});
+app.get('/api/v1/calculator/div/(((-?[0-9]+)[/]*))+', (req, res) => {
+    var numeros = req.params[0].split('/');
+    var resultado = numeros[0];
+    
+    for (var i = 1 ; i < numeros.length ; i++){
+        resultado = resultado / parseInt(numeros[i]);
+    }
+ 
+   res.send(resultado.toString());
+ });
+ app.get('/api/v1/calculator/subs/(((-?[0-9]+)[/]*))+', (req, res) => {
+    var numeros = req.params[0].split('/');
+    var resultado = 0;
+ 
+    for (var i in numeros){
+        resultado -= parseInt(numeros[i]);
+    }
+ 
+   res.send(resultado.toString());
+ });
+ app.get('/api/v1/calculator/mult/(((-?[0-9]+)[/]*))+', (req, res) => {
+    var numeros = req.params[0].split('/');
+    var resultado = 1;
+ 
+    for (var i in numeros){
+        resultado = resultado *  parseInt(numeros[i]);
+    }
+ 
+   res.send(resultado.toString());
+ });
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
